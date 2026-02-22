243a651c70dab4de21c73503ddaedf89a38bb0b7
ttbombadil <tom.tiltmann@th-koeln.de>
2026-02-22 12:18:56 +0100

    test: remove debug log from sandbox-ui-batching spec

--- e2e/sandbox-ui-batching.spec.ts
+++ e2e/sandbox-ui-batching.spec.ts
@@ -62,8 +62,6 @@ test.describe("Sandbox UI Batching Integration", () => {
 
     // Verifikation: Ist der Code im Editor? (Regex für Variablen + Zeilennummern)
     await monacoEditor.waitForReady();
-    // log current content to diagnose slow loading
-    console.log("Aktueller Editor-Inhalt:", await monacoEditor.getValue());
     // editor content may take a while to load; give it 30s
     await expect.poll(() => monacoEditor.getValue(), { timeout: 30000 }).toMatch(/\bpinMode\s*\(/i);
     await monacoEditor.verifyCodeContains("pinMode", { pin: 13, mode: "OUTPUT" });
